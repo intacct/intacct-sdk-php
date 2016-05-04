d275e83dedd41a74191446f024306e3b45e959c1
Coleen <cho@intacct.com>
2016-05-04 14:57:08 -0700

    Create read more test (#26)
    * Update read by query test (#24) (#22)
    
    * Fixing warnings
    * Fixing namespace
    
    * New unit test for ReadMore

---
+++ test/Intacct/Functions/ReadMoreTest.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Copyright 2016 Intacct Corporation.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. You may obtain a copy
+ *  of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "LICENSE" file accompanying this file. This file is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ */
+
+namespace Intacct\Functions;
+
+use Intacct\Xml\XMLWriter;
+use InvalidArgumentException;
+
+class ReadMoreTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers Intacct\Functions\ReadMore::__construct
+     * @covers Intacct\Functions\ReadMore::setControlId
+     * @covers Intacct\Functions\ReadMore::getXml
+     */
+    public function testGetXml()
+    {
+        $expected = <<<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<function controlid="unittest">
+    <readMore>
+        <resultId>6465763031VyprCMCoHYQAAGr@aRsAAAAU4</resultId>
+    </readMore>
+</function>
+EOF;
+
+        $xml = new XMLWriter();
+        $xml->openMemory();
+        $xml->setIndent(true);
+        $xml->setIndentString('    ');
+        $xml->startDocument();
+
+        $readMore = new ReadMore([
+            'result_id' => '6465763031VyprCMCoHYQAAGr@aRsAAAAU4',
+            'control_id' => 'unittest',
+        ]);
+        $readMore->getXml($xml);
+
+        $this->assertXmlStringEqualsXmlString($expected, $xml->flush());
+    }
+
+    /**
+     * @covers Intacct\Functions\ReadMore::__construct
+     * @covers Intacct\Functions\ReadMore::setControlId
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Required "result_id" key not supplied in params
+     */
+    public function testNoResultId()
+    {
+        new ReadMore([
+        ]);
+    }
+}
