f2d0dc3c41d7abd16883d63be3062022fa631be8
intacctcoleenho <cho@intacct.com>
2016-07-11 15:38:49 -0700

    Added CreateApAdjustment, CreateApAdjustmentEntry and unittests

---
+++ src/Functions/AccountsPayable/CreateApAdjustmentEntry.php
@@ -0,0 +1,249 @@
+<?php
+
+/**
+ * Copyright 2016 Intacct Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "LICENSE" file accompanying this file. This file is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+namespace Intacct\Functions\AccountsPayable;
+
+use Intacct\Functions\Traits\CustomFieldsTrait;
+use Intacct\Functions\Traits\DimensionsTrait;
+use InvalidArgumentException;
+use Intacct\Xml\XMLWriter;
+
+class CreateApAdjustmentEntry
+{
+    use DimensionsTrait;
+    use CustomFieldsTrait;
+
+    /**
+     * @var string
+     */
+    private $accountLabel;
+
+    /**
+     * @var string
+     */
+    private $glAccountNumber;
+
+    /**
+     * @var string
+     */
+    private $offsetGLAccountNumber;
+
+    /**
+     * @var string|float
+     */
+    private $transactionAmount;
+
+    /**
+     * @var string
+     */
+    private $allocationId;
+
+    /**
+     * @var string
+     */
+    private $memo;
+
+    /**
+     * @var bool
+     */
+    private $form1099;
+
+    /**
+     * @var string
+     */
+    private $key;
+
+    /**
+     * @var string|float
+     */
+    private $totalPaid;
+
+    /**
+     * @var string|float
+     */
+    private $totalDue;
+
+    /**
+     * @var bool
+     */
+    private $billable;
+
+    /**
+     *
+     * @param array $params my params
+     * @param string $functionControlId my function
+     * @throws InvalidArgumentException
+     */
+    public function __construct(array $params = [], $functionControlId = null)
+    {
+        $defaults = [
+            'account_label' => null,
+            'gl_account_no' => null,
+            'offset_gl_account_no' => null,
+            'transaction_amount' => null,
+            'allocation_id' => null,
+            'memo' => null,
+            'location_id' => null,
+            'department_id' => null,
+            'form_1099' => null,
+            'key' => null,
+            'total_paid' => null,
+            'total_due' => null,
+            'custom_fields' => [],
+            'project_id' => null,
+            'customer_id' => null,
+            'vendor_id' => null,
+            'employee_id' => null,
+            'item_id' => null,
+            'class_id' => null,
+            'contract_id' => null,
+            'warehouse_id' => null,
+            'billable' => null,
+        ];
+
+        $config = array_merge($defaults, $params);
+
+        $this->accountLabel = $config['account_label'];
+        $this->glAccountNumber = $config['gl_account_no'];
+        $this->offsetGLAccountNumber = $config['offset_gl_account_no'];
+        $this->setTransactionAmount($config['transaction_amount']);
+        $this->allocationId = $config['allocation_id'];
+        $this->memo = $config['memo'];
+        $this->setLocationId($config['location_id']);
+        $this->setDepartmentId($config['department_id']);
+        $this->setForm1099($config['form_1099']);
+        $this->key = $config['key'];
+        $this->setTotalPaid($config['total_paid']);
+        $this->setTotalDue($config['total_due']);
+        $this->setCustomFields($config['custom_fields']);
+        $this->setProjectId($config['project_id']);
+        $this->setCustomerId($config['customer_id']);
+        $this->setVendorId($config['vendor_id']);
+        $this->setEmployeeId($config['employee_id']);
+        $this->setItemId($config['item_id']);
+        $this->setClassId($config['class_id']);
+        $this->setContractId($config['contract_id']);
+        $this->setWarehouseId($config['warehouse_id']);
+        $this->setBillable($config['billable']);
+    }
+
+    /**
+     * @param $amount
+     * @throws InvalidArgumentException
+     */
+    private function setTransactionAmount($amount)
+    {
+        if (!is_numeric($amount)) {
+            throw new InvalidArgumentException('transaction_amount is not a valid number');
+        }
+
+        $this->transactionAmount = $amount;
+    }
+
+    /**
+     * @param $form1099
+     * @throws InvalidArgumentException
+     */
+    private function setForm1099($form1099)
+    {
+        if (!is_bool($form1099) && !is_null($form1099)) {
+            throw new InvalidArgumentException('form_1099 is not a valid bool');
+        }
+
+        $this->form1099 = $form1099;
+    }
+
+    /**
+     * @param $totalPaid
+     * @throws InvalidArgumentException
+     */
+    private function setTotalPaid($totalPaid)
+    {
+        if (!is_numeric($totalPaid) && !is_null($totalPaid)) {
+            throw new InvalidArgumentException('total_paid is not a valid number');
+        }
+
+        $this->totalPaid = $totalPaid;
+    }
+
+    /**
+     * @param $totalDue
+     * @throws InvalidArgumentException
+     */
+    private function setTotalDue($totalDue)
+    {
+        if (!is_numeric($totalDue) && !is_null($totalDue)) {
+            throw new InvalidArgumentException('total_due is not a valid number');
+        }
+
+        $this->totalDue = $totalDue;
+    }
+
+    /**
+     * @param $billable
+     * @throws InvalidArgumentException
+     */
+    private function setBillable($billable)
+    {
+        // Do we need to do any validation?
+        if (!is_bool($billable) && !is_null($billable)) {
+            throw new InvalidArgumentException('billable is not a valid bool');
+        }
+
+        $this->billable = $billable;
+    }
+
+    /**
+     * @param XMLWriter $xml
+     */
+    public function getXml(XMLWriter &$xml)
+    {
+        $xml->startElement('lineitem');
+
+
+        if (!is_null($this->accountLabel)) {
+            $xml->writeElement('accountlabel', $this->accountLabel, true);
+        } else {
+            $xml->writeElement('glaccountno', $this->glAccountNumber, true);
+        }
+
+        $xml->writeElement('offsetglaccountno', $this->offsetGLAccountNumber);
+        $xml->writeElement('amount', $this->transactionAmount, true);
+        $xml->writeElement('allocationid', $this->allocationId);
+        $xml->writeElement('memo' , $this->memo);
+        $xml->writeElement('locationid', $this->getLocationId());
+        $xml->writeElement('departmentid', $this->getDepartmentId());
+        $xml->writeElement('item1099', $this->form1099);
+        $xml->writeElement('key', $this->key);
+        $xml->writeElement('totalpaid', $this->totalPaid);
+        $xml->writeElement('totaldue', $this->totalDue);
+
+        $this->getCustomFieldsXml($xml);
+
+        $xml->writeElement('projectid', $this->getProjectId());
+        $xml->writeElement('customerid', $this->getCustomerId());
+        $xml->writeElement('vendorid', $this->getVendorId());
+        $xml->writeElement('employeeid', $this->getEmployeeId());
+        $xml->writeElement('itemid', $this->getItemId());
+        $xml->writeElement('classid', $this->getClassId());
+        $xml->writeElement('contractid', $this->getContractId());
+        $xml->writeElement('warehouseid', $this->getWarehouseId());
+        $xml->writeElement('billable', $this->billable);
+
+        $xml->endElement(); //lineitem
+    }
+}
---
+++ test/Intacct/Functions/AccountsPayable/CreateApAdjustmentEntryTest.php
@@ -0,0 +1,286 @@
+<?php
+
+/**
+ * Copyright 2016 Intacct Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "LICENSE" file accompanying this file. This file is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+namespace Intacct\Functions\AccountsPayable;
+
+use InvalidArgumentException;
+use Intacct\Xml\XMLWriter;
+
+class CreateApAdjustmentEntryTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::__construct
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setTransactionAmount
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::getXml
+     */
+    public function testDefaultParams()
+    {
+        $expected = <<<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<lineitem>
+    <glaccountno></glaccountno>
+    <amount>76343.43</amount>
+</lineitem>
+EOF;
+
+        $xml = new XMLWriter();
+        $xml->openMemory();
+        $xml->setIndent(true);
+        $xml->setIndentString('    ');
+        $xml->startDocument();
+
+        $apBillEntry = new CreateApAdjustmentEntry([
+            'transaction_amount' => 76343.43,
+        ]);
+        $apBillEntry->getXml($xml);
+
+        $this->assertXmlStringEqualsXmlString($expected, $xml->flush());
+    }
+
+    /**
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::__construct
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setTransactionAmount
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setLocationId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::getLocationId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setDepartmentId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::getDepartmentId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setForm1099
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setTotalPaid
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setTotalDue
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setCustomFields
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::getCustomFieldsXml
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setProjectId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::getProjectId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setCustomerId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::getCustomerId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setVendorId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::getVendorId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setEmployeeId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::getEmployeeId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setItemId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::getItemId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setClassId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::getClassId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setContractId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::getContractId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setWarehouseId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::getWarehouseId
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setBillable
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::getXml
+     */
+    public function testParamOverrides()
+    {
+        $expected = <<<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<lineitem>
+    <accountlabel>TestBill Account1</accountlabel>
+    <offsetglaccountno>93590253</offsetglaccountno>
+    <amount>76343.43</amount>
+    <allocationid>89cv23589</allocationid>
+    <memo>Just another memo</memo>
+    <locationid>fuion23iouhg443f23f232</locationid>
+    <departmentid>Test Department</departmentid>
+    <item1099>true</item1099>
+    <key>Key1</key>
+    <totalpaid>23484.93</totalpaid>
+    <totaldue>0</totaldue>
+    <customfields>
+        <customfield>
+            <customfieldname>customfield1</customfieldname>
+            <customfieldvalue>customvalue1</customfieldvalue>
+        </customfield>
+    </customfields>
+    <projectid>Project1</projectid>
+    <customerid>Customer1</customerid>
+    <vendorid>Vendor1</vendorid>
+    <employeeid>Employee1</employeeid>
+    <itemid>Item1</itemid>
+    <classid>Class1</classid>
+    <warehouseid>Warehouse1</warehouseid>
+    <billable>true</billable>
+</lineitem>
+EOF;
+
+        $xml = new XMLWriter();
+        $xml->openMemory();
+        $xml->setIndent(true);
+        $xml->setIndentString('    ');
+        $xml->startDocument();
+
+        $apBillEntry = new CreateApAdjustmentEntry([
+            'account_label' => 'TestBill Account1',
+            'offset_gl_account_no' => 93590253,
+            'transaction_amount' => 76343.43,
+            'allocation_id' => '89cv23589',
+            'memo' => 'Just another memo',
+            'location_id' => 'fuion23iouhg443f23f232',
+            'department_id' => 'Test Department',
+            'form_1099' => true,
+            'key' => 'Key1',
+            'total_paid' => 23484.93,
+            'total_due' => 0.00,
+            'custom_fields' => [
+                'customfield1' => 'customvalue1'
+            ],
+            'project_id' => 'Project1',
+            'customer_id' => 'Customer1',
+            'vendor_id' => 'Vendor1',
+            'employee_id' => 'Employee1',
+            'item_id' => 'Item1',
+            'class_id' => 'Class1',
+            'warehouse_id' => 'Warehouse1',
+            'billable' => true,
+        ]);
+        $apBillEntry->getXml($xml);
+
+        $this->assertXmlStringEqualsXmlString($expected, $xml->flush());
+    }
+
+    /**
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::__construct
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setTransactionAmount
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::getXml
+     */
+    public function testGLAccountNumber()
+    {
+        $expected = <<<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<lineitem>
+    <glaccountno>23453252353</glaccountno>
+    <amount>234.26</amount>
+</lineitem>
+EOF;
+
+        $xml = new XMLWriter();
+        $xml->openMemory();
+        $xml->setIndent(true);
+        $xml->setIndentString('    ');
+        $xml->startDocument();
+
+        $apBillEntry = new CreateApAdjustmentEntry([
+            'gl_account_no' => 23453252353,
+            'transaction_amount' => 234.26
+        ]);
+        $apBillEntry->getXml($xml);
+
+        $this->assertXmlStringEqualsXmlString($expected, $xml->flush());
+    }
+
+    /**
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::__construct
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setTransactionAmount
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage transaction_amount is not a valid number
+     */
+    public function testMissingAmount()
+    {
+        $xml = new XMLWriter();
+        $xml->openMemory();
+        $xml->setIndent(true);
+        $xml->setIndentString('    ');
+        $xml->startDocument();
+
+        new CreateApAdjustmentEntry([
+        ]);
+    }
+
+    /**
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::__construct
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setTransactionAmount
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setForm1099
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage form_1099 is not a valid bool
+     */
+    public function testInvalidForm1099()
+    {
+        $xml = new XMLWriter();
+        $xml->openMemory();
+        $xml->setIndent(true);
+        $xml->setIndentString('    ');
+        $xml->startDocument();
+
+        new CreateApAdjustmentEntry([
+            'transaction_amount' => 234.26,
+            'form_1099' => '2902',
+        ]);
+    }
+
+    /**
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::__construct
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setTransactionAmount
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setTotalPaid
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage total_paid is not a valid number
+     */
+    public function testInvalidTotalPaid()
+    {
+        $xml = new XMLWriter();
+        $xml->openMemory();
+        $xml->setIndent(true);
+        $xml->setIndentString('    ');
+        $xml->startDocument();
+
+        new CreateApAdjustmentEntry([
+            'transaction_amount' => 234.26,
+            'total_paid' => '$2902',
+        ]);
+    }
+
+    /**
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::__construct
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setTransactionAmount
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setTotalDue
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage total_due is not a valid number
+     */
+    public function testInvalidTotalDue()
+    {
+        $xml = new XMLWriter();
+        $xml->openMemory();
+        $xml->setIndent(true);
+        $xml->setIndentString('    ');
+        $xml->startDocument();
+
+        new CreateApAdjustmentEntry([
+            'transaction_amount' => 234.26,
+            'total_due' => true,
+        ]);
+    }
+
+    /**
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::__construct
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setTransactionAmount
+     * @covers Intacct\Functions\AccountsPayable\CreateApAdjustmentEntry::setBillable
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage billable is not a valid bool
+     */
+    public function testInvalidBillable()
+    {
+        $xml = new XMLWriter();
+        $xml->openMemory();
+        $xml->setIndent(true);
+        $xml->setIndentString('    ');
+        $xml->startDocument();
+
+        new CreateApAdjustmentEntry([
+            'transaction_amount' => 234.26,
+            'billable' => 'true',
+        ]);
+    }
+
+}
