036f1dc4378394d104b10704dbd8e48a4a005956
James McGonegal <jmcgonegal@intacct.com>
2016-08-22 12:31:25 -0700

    Add delete function for statistical account object #43

---
+++ src/Intacct/Functions/GeneralLedger/StatisticalAccountDelete.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Copyright 2016 Intacct Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "LICENSE" file accompanying this file. This file is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+namespace Intacct\Functions\GeneralLedger;
+
+use Intacct\Xml\XMLWriter;
+use InvalidArgumentException;
+
+/**
+ * Delete an existing statistical account record
+ */
+class StatisticalAccountDelete extends AbstractStatisticalAccount
+{
+
+    /**
+     * Write the function block XML
+     *
+     * @param XMLWriter $xml
+     * @throw InvalidArgumentException
+     */
+    public function writeXml(XMLWriter &$xml)
+    {
+        $xml->startElement('function');
+        $xml->writeAttribute('controlid', $this->getControlId());
+
+        $xml->startElement('delete');
+        $xml->writeElement('object', 'STATACCOUNT');
+
+        if (!$this->getAccountNo()) {
+            throw new InvalidArgumentException('Account No is required for delete');
+        }
+        $xml->writeElement('keys', $this->getAccountNo(), true);
+
+        $xml->endElement(); //delete
+
+        $xml->endElement(); //function
+    }
+}
---
+++ test/Intacct/Functions/GeneralLedger/StatisticalAccountDeleteTest.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Copyright 2016 Intacct Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "LICENSE" file accompanying this file. This file is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+namespace Intacct\Functions\GeneralLedger;
+
+use Intacct\Xml\XMLWriter;
+use InvalidArgumentException;
+
+class StatisticalAccountDeleteTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @covers Intacct\Functions\GeneralLedger\StatisticalAccountDelete::writeXml
+     */
+    public function testConstruct()
+    {
+        $expected = <<<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<function controlid="unittest">
+    <delete>
+        <object>STATACCOUNT</object>
+        <keys>9000</keys>
+    </delete>
+</function>
+EOF;
+
+        $xml = new XMLWriter();
+        $xml->openMemory();
+        $xml->setIndent(true);
+        $xml->setIndentString('    ');
+        $xml->startDocument();
+
+        $classObj = new StatisticalAccountDelete('unittest');
+        $classObj->setAccountNo('9000');
+
+        $classObj->writeXml($xml);
+
+        $this->assertXmlStringEqualsXmlString($expected, $xml->flush());
+    }
+
+    /**
+     * @covers Intacct\Functions\GeneralLedger\StatisticalAccountDelete::writeXml
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Account No is required for delete
+     */
+    public function testRequiredId()
+    {
+        $xml = new XMLWriter();
+        $xml->openMemory();
+        $xml->setIndent(true);
+        $xml->setIndentString('    ');
+        $xml->startDocument();
+
+        $obj = new StatisticalAccountDelete('unittest');
+
+        $obj->writeXml($xml);
+    }
+}
