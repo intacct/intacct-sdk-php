b5d2ac4f425940a2ae507c3f3a7c2013a25acaf5
James McGonegal <jmcgonegal@intacct.com>
2016-08-22 12:36:41 -0700

    Add delete function for project object

---
+++ test/Intacct/Functions/Projects/ProjectDeleteTest.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Copyright 2016 Intacct Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "LICENSE" file accompanying this file. This file is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+namespace Intacct\Functions\Projects;
+
+use Intacct\Xml\XMLWriter;
+use InvalidArgumentException;
+
+class ProjectDeleteTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @covers Intacct\Functions\Projects\ProjectDelete::writeXml
+     */
+    public function testConstruct()
+    {
+        $expected = <<<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<function controlid="unittest">
+    <delete>
+        <object>PROJECT</object>
+        <keys>P1234</keys>
+    </delete>
+</function>
+EOF;
+
+        $xml = new XMLWriter();
+        $xml->openMemory();
+        $xml->setIndent(true);
+        $xml->setIndentString('    ');
+        $xml->startDocument();
+
+        $classObj = new ProjectDelete('unittest');
+        $classObj->setProjectId('P1234');
+
+        $classObj->writeXml($xml);
+
+        $this->assertXmlStringEqualsXmlString($expected, $xml->flush());
+    }
+
+    /**
+     * @covers Intacct\Functions\Projects\ProjectDelete::writeXml
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Project ID is required for delete
+     */
+    public function testRequiredId()
+    {
+        $xml = new XMLWriter();
+        $xml->openMemory();
+        $xml->setIndent(true);
+        $xml->setIndentString('    ');
+        $xml->startDocument();
+
+        $obj = new ProjectDelete('unittest');
+
+        $obj->writeXml($xml);
+    }
+}
