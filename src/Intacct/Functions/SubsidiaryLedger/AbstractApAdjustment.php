851c0b91b8d2a296910e72da6bc57888d501d124
James McGonegal <jmcgonegal@intacct.com>
2016-08-22 13:34:31 -0700

    Add delete function for AP adjustment object #43

--- src/Intacct/Functions/SubsidiaryLedger/AbstractApAdjustment.php
+++ src/Intacct/Functions/SubsidiaryLedger/AbstractApAdjustment.php
@@ -27,6 +27,9 @@ abstract class AbstractApAdjustment extends AbstractFunction
 
     use CustomFieldsTrait;
 
+    /** @var int */
+    protected $recordNo;
+
     /** @var string */
     protected $vendorId;
 
@@ -77,6 +80,22 @@ abstract class AbstractApAdjustment extends AbstractFunction
 
     /** @var array */
     protected $lines;
+
+    /**
+     * @return int|string
+     */
+    public function getRecordNo()
+    {
+        return $this->recordNo;
+    }
+
+    /**
+     * @param int|string $recordNo
+     */
+    public function setRecordNo($recordNo)
+    {
+        $this->recordNo = $recordNo;
+    }
 
     /**
      * @return string
