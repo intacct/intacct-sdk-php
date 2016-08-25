01ab0aa65f3b2cddbe0cb4b6ff1bf4550adff2d5
James McGonegal <jmcgonegal@intacct.com>
2016-08-24 18:52:28 -0700

    Add Attachments object functions #46

---
+++ src/Intacct/Functions/Company/AttachmentsUpdate.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Copyright 2016 Intacct Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "LICENSE" file accompanying this file. This file is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+namespace Intacct\Functions\Company;
+
+use Intacct\Xml\XMLWriter;
+use InvalidArgumentException;
+
+/**
+ * Update an existing attachments record
+ */
+class AttachmentsUpdate extends AbstractAttachments
+{
+
+    /**
+     * Write the function block XML
+     *
+     * @param XMLWriter $xml
+     * @throw InvalidArgumentException
+     */
+    public function writeXml(XMLWriter &$xml)
+    {
+        $xml->startElement('function');
+        $xml->writeAttribute('controlid', $this->getControlId());
+
+        $xml->startElement('update_supdoc');
+
+        if (!$this->getAttachmentsId()) {
+            throw new InvalidArgumentException('Attachments ID is required for update');
+        }
+        $xml->writeElement('supdocid', $this->getAttachmentsId(), true);
+
+        $xml->writeElement('supdocname', $this->getAttachmentsName());
+        $xml->writeElement('supdocfoldername', $this->getAttachmentFolderName());
+        $xml->writeElement('supdocdescription', $this->getDescription());
+
+        if (count($this->getFiles()) > 0) {
+            $xml->startElement('attachments');
+
+            foreach ($this->getFiles() as $file) {
+                if ($file instanceof AttachmentInterface) {
+                    $file->writeXml($xml);
+                }
+            }
+
+            $xml->endElement(); //attachments
+        }
+
+        $xml->endElement(); //update_supdoc
+
+        $xml->endElement(); //function
+    }
+}
---
+++ test/Intacct/Functions/Company/AttachmentsDeleteTest.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Copyright 2016 Intacct Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "LICENSE" file accompanying this file. This file is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+namespace Intacct\Functions\Company;
+
+use Intacct\Xml\XMLWriter;
+use InvalidArgumentException;
+
+class AttachmentsDeleteTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @covers Intacct\Functions\Company\AttachmentsDelete::writeXml
+     */
+    public function testConstruct()
+    {
+        $expected = <<<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<function controlid="unittest">
+    <delete_supdoc key="A1234" />
+</function>
+EOF;
+
+        $xml = new XMLWriter();
+        $xml->openMemory();
+        $xml->setIndent(true);
+        $xml->setIndentString('    ');
+        $xml->startDocument();
+
+        $record = new AttachmentsDelete('unittest');
+        $record->setAttachmentsId('A1234');
+
+        $record->writeXml($xml);
+
+        $this->assertXmlStringEqualsXmlString($expected, $xml->flush());
+    }
+
+    /**
+     * @covers Intacct\Functions\Company\AttachmentsDelete::writeXml
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Attachments ID is required for delete
+     */
+    public function testRequiredId()
+    {
+        $xml = new XMLWriter();
+        $xml->openMemory();
+        $xml->setIndent(true);
+        $xml->setIndentString('    ');
+        $xml->startDocument();
+
+        $record = new AttachmentsDelete('unittest');
+
+        $record->writeXml($xml);
+    }
+}
