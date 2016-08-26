681497758822f9acbeaeb3ed9984a179cafb688e
James <james.mcgonegal@gmail.com>
2016-08-26 08:01:48 -0700

    Add CM Charge Card Transaction object functions #36

---
+++ src/Intacct/Functions/SubsidiaryLedger/AbstractCmChargeCardTransactionLine.php
@@ -0,0 +1,295 @@
+<?php
+
+/**
+ * Copyright 2016 Intacct Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "LICENSE" file accompanying this file. This file is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+namespace Intacct\Functions\SubsidiaryLedger;
+
+use Intacct\Functions\Traits\CustomFieldsTrait;
+use Intacct\Xml\XMLWriter;
+
+abstract class AbstractCmChargeCardTransactionLine
+{
+
+    use CustomFieldsTrait;
+
+    /** @var string */
+    protected $accountLabel;
+
+    /** @var string */
+    protected $glAccountNumber;
+
+    /** @var string|float */
+    protected $transactionAmount;
+
+    /** @var string */
+    protected $memo;
+
+    /** @var string */
+    protected $departmentId;
+
+    /** @var string */
+    protected $locationId;
+
+    /** @var string */
+    protected $projectId;
+
+    /** @var string */
+    protected $customerId;
+
+    /** @var string */
+    protected $vendorId;
+
+    /** @var string */
+    protected $employeeId;
+
+    /** @var string */
+    protected $itemId;
+
+    /** @var string */
+    protected $classId;
+
+    /** @var string */
+    protected $contractId;
+
+    /** @var string */
+    protected $warehouseId;
+
+    /**
+     * @return string
+     */
+    public function getAccountLabel()
+    {
+        return $this->accountLabel;
+    }
+
+    /**
+     * @param string $accountLabel
+     */
+    public function setAccountLabel($accountLabel)
+    {
+        $this->accountLabel = $accountLabel;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGlAccountNumber()
+    {
+        return $this->glAccountNumber;
+    }
+
+    /**
+     * @param string $glAccountNumber
+     */
+    public function setGlAccountNumber($glAccountNumber)
+    {
+        $this->glAccountNumber = $glAccountNumber;
+    }
+
+    /**
+     * @return float|string
+     */
+    public function getTransactionAmount()
+    {
+        return $this->transactionAmount;
+    }
+
+    /**
+     * @param float|string $transactionAmount
+     */
+    public function setTransactionAmount($transactionAmount)
+    {
+        $this->transactionAmount = $transactionAmount;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMemo()
+    {
+        return $this->memo;
+    }
+
+    /**
+     * @param string $memo
+     */
+    public function setMemo($memo)
+    {
+        $this->memo = $memo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDepartmentId()
+    {
+        return $this->departmentId;
+    }
+
+    /**
+     * @param string $departmentId
+     */
+    public function setDepartmentId($departmentId)
+    {
+        $this->departmentId = $departmentId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocationId()
+    {
+        return $this->locationId;
+    }
+
+    /**
+     * @param string $locationId
+     */
+    public function setLocationId($locationId)
+    {
+        $this->locationId = $locationId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProjectId()
+    {
+        return $this->projectId;
+    }
+
+    /**
+     * @param string $projectId
+     */
+    public function setProjectId($projectId)
+    {
+        $this->projectId = $projectId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCustomerId()
+    {
+        return $this->customerId;
+    }
+
+    /**
+     * @param string $customerId
+     */
+    public function setCustomerId($customerId)
+    {
+        $this->customerId = $customerId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVendorId()
+    {
+        return $this->vendorId;
+    }
+
+    /**
+     * @param string $vendorId
+     */
+    public function setVendorId($vendorId)
+    {
+        $this->vendorId = $vendorId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmployeeId()
+    {
+        return $this->employeeId;
+    }
+
+    /**
+     * @param string $employeeId
+     */
+    public function setEmployeeId($employeeId)
+    {
+        $this->employeeId = $employeeId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getItemId()
+    {
+        return $this->itemId;
+    }
+
+    /**
+     * @param string $itemId
+     */
+    public function setItemId($itemId)
+    {
+        $this->itemId = $itemId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClassId()
+    {
+        return $this->classId;
+    }
+
+    /**
+     * @param string $classId
+     */
+    public function setClassId($classId)
+    {
+        $this->classId = $classId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContractId()
+    {
+        return $this->contractId;
+    }
+
+    /**
+     * @param string $contractId
+     */
+    public function setContractId($contractId)
+    {
+        $this->contractId = $contractId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getWarehouseId()
+    {
+        return $this->warehouseId;
+    }
+
+    /**
+     * @param string $warehouseId
+     */
+    public function setWarehouseId($warehouseId)
+    {
+        $this->warehouseId = $warehouseId;
+    }
+
+    abstract public function writeXml(XMLWriter &$xml);
+}
