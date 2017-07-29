2c67ee88969a69c145b5d664130d75657172bddf
James McGonegal <jimmymcpeter@users.noreply.github.com>
2017-07-29 10:49:09 -0700

    Clean up MessageFormatter docs for doxygen

--- src/Intacct/Logging/MessageFormatter.php
+++ src/Intacct/Logging/MessageFormatter.php
@@ -20,17 +20,39 @@ namespace Intacct\Logging;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
-/**
- * @inheritdoc
- */
 final class MessageFormatter extends \GuzzleHttp\MessageFormatter
 {
 
-    /** @var string */
-    const REPLACEMENT = '$1REDACTED$3';
+    const CLF = "{hostname} {req_header_User-Agent} - [{date_common_log}] \"{method} {target} HTTP/{version}\" {code} {res_header_Content-Length}";
+    const DEBUG = ">>>>>>>>\n{request}\n<<<<<<<<\n{response}\n--------\n{error}";
+    const SHORT = '[{ts}] "{method} {target} HTTP/{version}" {code}';
 
     /**
-     * @inheritdoc
+     * Formats log messages using variable substitutions for requests, responses,
+     * and other transactional data.
+     *
+     * @param string $template The following variable substitutions are supported:
+     * - `{request}`: Full HTTP request message
+     * - `{response}`: Full HTTP response message
+     * - `{ts}`: ISO 8601 date in GMT
+     * - `{date_iso_8601}`: ISO 8601 date in GMT
+     * - `{date_common_log}`: Apache common log date using the configured timezone.
+     * - `{host}`: Host of the request
+     * - `{method}`: Method of the request
+     * - `{uri}`: URI of the request
+     * - `{host}`: Host of the request
+     * - `{version}`: Protocol version
+     * - `{target}`: Request target of the request (path + query + fragment)
+     * - `{hostname}`: Hostname of the machine that sent the request
+     * - `{code}`: Status code of the response (if available)
+     * - `{phrase}`: Reason phrase of the response  (if available)
+     * - `{error}`: Any error messages (if available)
+     * - `{req_header_*}`: Replace `*` with the lowercased name of a request header to add to the message
+     * - `{res_header_*}`: Replace `*` with the lowercased name of a response header to add to the message
+     * - `{req_headers}`: Request headers
+     * - `{res_headers}`: Response headers
+     * - `{req_body}`: Request body
+     * - `{res_body}`: Response body
      */
     public function __construct($template = self::DEBUG)
     {
@@ -47,32 +69,34 @@ final class MessageFormatter extends \GuzzleHttp\MessageFormatter
     ) {
         $message = parent::format($request, $response, $error);
 
+        $replacement = '$1REDACTED$3';
+
         $patterns = [];
         $replacements = [];
 
         $patterns[] = '/(<password[^>]*>)(.*?)(<\/password>)/i';
-        $replacements[] = static::REPLACEMENT;
+        $replacements[] = $replacement;
 
         $patterns[] = '/(<sessionid[^>]*>)(.*?)(<\/sessionid>)/i';
-        $replacements[] = static::REPLACEMENT;
+        $replacements[] = $replacement;
 
         $patterns[] = '/(<accountnumber[^>]*>)(.*?)(<\/accountnumber>)/i';
-        $replacements[] = static::REPLACEMENT;
+        $replacements[] = $replacement;
 
         $patterns[] = '/(<cardnum[^>]*>)(.*?)(<\/cardnum>)/i';
-        $replacements[] = static::REPLACEMENT;
+        $replacements[] = $replacement;
 
         $patterns[] = '/(<ssn[^>]*>)(.*?)(<\/ssn>)/i';
-        $replacements[] = static::REPLACEMENT;
+        $replacements[] = $replacement;
 
         $patterns[] = '/(<achaccountnumber[^>]*>)(.*?)(<\/achaccountnumber>)/i';
-        $replacements[] = static::REPLACEMENT;
+        $replacements[] = $replacement;
 
         $patterns[] = '/(<wireaccountnumber[^>]*>)(.*?)(<\/wireaccountnumber>)/i';
-        $replacements[] = static::REPLACEMENT;
+        $replacements[] = $replacement;
 
         $patterns[] = '/(<taxid[^>]*>)(.*?)(<\/taxid>)/i';
-        $replacements[] = static::REPLACEMENT;
+        $replacements[] = $replacement;
 
         return preg_replace(
             $patterns,
