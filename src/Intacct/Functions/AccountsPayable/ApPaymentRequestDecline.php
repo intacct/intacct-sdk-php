6b5bb7463c86d24b866edf887d06e6505c42c39f
Coleen Ho <coleen.ho@sage.com>
2020-09-22 14:37:21 -0700

    Add Decline AP Payements Request

---
+++ src/Intacct/Functions/AccountsPayable/ApPaymentRequestDecline.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Copyright 2020 Sage Intacct, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "LICENSE" file accompanying this file. This file is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+namespace Intacct\Functions\AccountsPayable;
+
+use Intacct\Xml\XMLWriter;
+use InvalidArgumentException;
+
+/*
+ * Decline an existing AP payment request record
+ */
+class ApPaymentRequestDecline extends AbstractApPaymentRequest
+{
+    /**
+     * Write the function block XML
+     *
+     * @param XMLWriter $xml
+     * @throw InvalidArgumentException
+     */
+    public function writeXml(XMLWriter &$xml)
+    {
+        $xml->startElement('function');
+        $xml->writeAttribute('controlid', $this->getControlId());
+
+        $xml->startElement('decline_appaymentrequest');
+
+        if (!$this->getRecordNo()) {
+            throw new InvalidArgumentException('Record No is required for decline');
+        }
+        $xml->writeAttribute('key', $this->getRecordNo());
+
+        $xml->endElement(); //decline_appaymentrequest
+
+        $xml->endElement(); //function
+    }
+}
