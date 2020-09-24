aebd7e86c382d1ac5915bcc5a9754f3943b5db0b
Coleen Ho <coleen.ho@sage.com>
2020-09-24 13:14:30 -0700

    Add approve and decline for PurchasingTransactions #144

---
+++ test/Intacct/Functions/Purchasing/PurchasingTransactionApproveTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Copyright 2020 Sage Intacct, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "LICENSE" file accompanying this file. This file is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+namespace Intacct\Functions\Purchasing;
+
+use Intacct\Xml\XMLWriter;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \Intacct\Functions\Purchasing\PurchasingTransactionCreate
+ */
+class PurchasingTransactionApproveTest extends TestCase
+{
+
+    public function testDefaultParams()
+    {
+
+        $expected = <<<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<function controlid="unittest">
+    <approve>
+        <PODOCUMENT>
+            <DOCID>Purchase Order-PO0213</DOCID>
+            <COMMENT>Final approval for the quarter</COMMENT>
+        </PODOCUMENT>
+    </approve>
+</function>
+EOF;
+
+        $xml = new XMLWriter();
+        $xml->openMemory();
+        $xml->setIndent(true);
+        $xml->setIndentString('    ');
+        $xml->startDocument();
+
+        $transaction = new PurchasingTransactionApprove('unittest');
+        $transaction->setExternalId('Purchase Order-PO0213');
+        $transaction->setComment('Final approval for the quarter');
+
+        $transaction->writeXml($xml);
+
+        self::assertXmlStringEqualsXmlString($expected, $xml->flush());
+    }
+}
